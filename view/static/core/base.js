d48fd76756b24ab667f682d03bc7ee666454a61a
travel0891 <travel4ever@yeah.net>
2016-02-20 11:19:29 +0800

    begin

---
+++ view/static/core/base.js
@@ -0,0 +1,7 @@
+﻿var outer = {};
+
+outer.toLogin = function() {
+    var html = "<a class=\"btn\" href=\"javascript:;\">已完成支付</a><a class=\"btn\" href=\"javascript:void(0);\">支付遇到问题</a>";
+    $("#message").css("text-align", "center");
+    $("#message").html(html);
+};
